34f9439e43caa60c0bcbd6830b425b278f9064e7
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-03-07 22:22:44 -0300

    a bit more organized

--- led_ir/led_ir.py
+++ led_ir/led_ir.py
@@ -6,18 +6,27 @@ import RPi.GPIO as GPIO
 
 LED = 12
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, GPIO.HIGH)
 
-try:
-    while True:
-        print "acende"
+def emissor():
+        print "on"
         GPIO.output(LED, GPIO.HIGH)
         time.sleep(1.5)
-        print "apage"
+        print "off"
         GPIO.output(LED, GPIO.LOW)
         time.sleep(1.5)
-except:
+
+
+def main():
+    GPIO.setmode(GPIO.BOARD)
     GPIO.cleanup()
+    GPIO.setup(LED, GPIO.OUT)
+    GPIO.output(LED, GPIO.HIGH)
+    try:
+        while True:
+            emissor()
+    except:
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
