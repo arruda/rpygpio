4e82826e4778aec701bb3b63f83b01f68449d43a
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-03-07 23:24:38 -0300

    gpio layout, and led receptor test(trying to read analog using digital only)

--- led_ir/led_ir.py
+++ led_ir/led_ir.py
@@ -5,15 +5,30 @@ import time
 import RPi.GPIO as GPIO
 
 LED = 12
+RECEPTOR = 26
 
 
-def emissor():
-        print "on"
-        GPIO.output(LED, GPIO.HIGH)
-        time.sleep(1.5)
-        print "off"
-        GPIO.output(LED, GPIO.LOW)
-        time.sleep(1.5)
+# Define function to measure charge time
+def rc_analog(pin):
+    counter = 0
+    # Discharge capacitor
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.setup(pin, GPIO.IN)
+    # Count loops until voltage across capacitor reads high on GPIO
+    while GPIO.input(pin) == GPIO.LOW:
+        counter = counter + 1
+    return counter
+
+
+# def emitter():
+#         print "on"
+#         GPIO.output(LED, GPIO.HIGH)
+#         time.sleep(1.5)
+#         print "off"
+#         GPIO.output(LED, GPIO.LOW)
+#         time.sleep(1.5)
 
 
 def main():
@@ -21,10 +36,14 @@ def main():
     GPIO.cleanup()
     GPIO.setup(LED, GPIO.OUT)
     GPIO.output(LED, GPIO.HIGH)
+
     try:
         while True:
-            emissor()
+            print rc_analog(RECEPTOR)  # Measure timing using GPIO pin
     except:
+        pass
+    finally:
+        print "exit"
         GPIO.cleanup()
 
 
