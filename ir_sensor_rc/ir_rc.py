9da0b7674d5d2a4d2c7aa419ed59194eff88ac27
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-04-01 21:52:46 -0300

    trying the same as before, but with IR receiver

---
+++ ir_sensor_rc/ir_rc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+
+import RPi.GPIO as GPIO
+import time
+
+DEBUG = 1
+GPIO.setmode(GPIO.BOARD)
+
+READING_LIMIT = 9000000  # could even do some calibration
+
+
+def rc_time(rc_pin):
+        reading = 0
+        GPIO.setup(rc_pin, GPIO.OUT)
+        GPIO.output(rc_pin, GPIO.LOW)
+        time.sleep(0.05)
+
+        GPIO.setup(rc_pin, GPIO.IN)
+        while (GPIO.input(rc_pin) == GPIO.LOW):
+                reading += 1
+        return reading
+
+
+def main(rc_pin):
+    print "starting"
+    while True:
+        print rc_time(rc_pin)
+
+
+if __name__ == '__main__':
+    GPIO.cleanup()
+    try:
+        main(18)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
