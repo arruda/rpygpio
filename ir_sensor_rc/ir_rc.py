076872eb6fb25e2adf4c6999aacc012739b2199e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-04-17 23:09:45 -0300

    doing some manual calibration

--- ir_sensor_rc/ir_rc.py
+++ ir_sensor_rc/ir_rc.py
@@ -10,10 +10,21 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-READING_LIMIT = 1200  #could even do some calibration
+READING_LIMIT = 940  #could even do some calibration
+READING_LIMIT_CM = 32
 
 # 'ignore' noise
-SIGMA = READING_LIMIT / 40
+SIGMA = READING_LIMIT / READING_LIMIT_CM
+
+
+def check_inside_area(value, max_value, min_value):
+    return min_value < value < max_value
+
+
+def rc_to_cm(rc_value, rc_max, cm_max):
+    conversion_rate = float(cm_max) / rc_max
+    cm_value = rc_value * conversion_rate
+    return cm_value
 
 
 def rc_time_with_sigma(rc_pin, sigma):
@@ -23,6 +34,10 @@ def rc_time_with_sigma(rc_pin, sigma):
     while distance < sigma:
         current_reading = rc_time(rc_pin)
         distance = abs(last_reading - current_reading)
+        cm_value = rc_to_cm(last_reading, READING_LIMIT, READING_LIMIT_CM)
+        print cm_value
+        if check_inside_area(cm_value, 30, 20):
+            print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     last_reading = current_reading
     return last_reading
 
@@ -42,7 +57,9 @@ def rc_time(rc_pin):
 def main(rc_pin):
     print "starting"
     while True:
-        print rc_time_with_sigma(rc_pin, SIGMA)
+        rc_value = rc_time_with_sigma(rc_pin, SIGMA)
+        cm_value = rc_to_cm(rc_value, READING_LIMIT, READING_LIMIT_CM)
+        print cm_value
 
 
 if __name__ == '__main__':
