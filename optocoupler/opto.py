75ef4f7a2f8c831ce48be76ff7c408585773ef10
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-04-21 00:59:37 -0300

    small fixes

--- optocoupler/opto.py
+++ optocoupler/opto.py
@@ -4,20 +4,21 @@
 
 import RPi.GPIO as GPIO
 import time
-rc_pin = 18
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(rc_pin, GPIO.OUT)
 
 
-def main():
+def main(rc_pin):
+    GPIO.setup(rc_pin, GPIO.OUT)
     while True:
-        GPIO.output(rc_pin, GPIO.HIGHT)
+        GPIO.output(rc_pin, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(rc_pin, GPIO.LOW)
         time.sleep(0.5)
 
 
 if __name__ == '__main__':
+    print 'will start in 5 seconds.'
+    time.sleep(5)
     try:
         main(18)
     except KeyboardInterrupt:
